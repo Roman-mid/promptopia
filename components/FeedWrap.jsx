1765636bd8f6d5035c383e0b8e548e5fc7d78974
Roman-mid <romanmidianko@gmail.com>
2025-08-09 20:52:29 +0100

    change lint to get prompts

--- components/FeedWrap.jsx
+++ components/FeedWrap.jsx
@@ -1,8 +1,9 @@
 import Feed from './Feed';
 
 const FeedWrap = async () => {
-  const response = await fetch('http://localhost:3000/api/prompt');
-  // const response = await fetch('https://promptopia-xi-henna.vercel.app/api/prompt');
+  const response = await fetch(
+    'https://promptopia-xi-henna.vercel.app/api/prompt'
+  );
   const posts = await response.json();
 
   return <Feed posts={posts.reverse()} />;
