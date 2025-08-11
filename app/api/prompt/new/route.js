79e408e1ea0bf24de66e332360997d3c10fa96ef
Roman-mid <romanmidianko@gmail.com>
2025-08-11 12:59:31 +0100

    json response error in api/create-prompt/route

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -4,6 +4,8 @@ import Prompt from '@models/prompt';
 export const POST = async (req) => {
   const { userId, prompt, tag } = await req.json();
 
+  console.log({ userId, prompt, tag });
+
   try {
     await connectToDB();
     const newPrompt = new Prompt({
@@ -18,8 +20,11 @@ export const POST = async (req) => {
       status: 201,
     });
   } catch (error) {
-    return new Response('Failed to create a new prompt', {
-      status: 500,
-    });
+    return new Response(
+      JSON.stringify({ error: 'Failed to create a new prompt' }),
+      {
+        status: 500,
+      }
+    );
   }
 };
